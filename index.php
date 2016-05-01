96cdccdb56d8876be4de102dcb8b6630ac552d4d
Tuvshinbat <ggakillis@yahoo.com>
2016-05-01 10:39:22 +0800

    test
    aa

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';sdfsdfsdf
-	} else {sdfsdf
-		$uri = 'http://';sdf
-	}sdfs
-	$uri .= $_SERVER['HTTP_HOST'];s
-	exit;dfssd
-?>sdf
-Something is wrong witfsdfsdfh the XAMPP installation :-(sdf
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	exit;
+?>
+Something is wrong the XAMPP installation :-(
